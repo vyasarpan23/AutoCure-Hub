0a6061c7036c2005d240820216ac02b76571f0ed
manasviajmera <manasviajmera0711@gmail.com>
2025-03-15 13:52:07 +0530

    cart and checkout page added ,for the service booking

--- server/db.js
+++ server/db.js
@@ -5,16 +5,16 @@ dotenv.config(); // Load environment variables from .env file
 
 // Create a MySQL connection pool (for better connection management)
 const pool = mysql.createPool({
-    host: process.env.DB_HOST, // Load from .env
-    user: process.env.DB_USER, // Load from .env
-    password: process.env.DB_PASS, // Load from .env
-    database: process.env.DB_NAME, // Load from .env
+    host: process.env.DB_HOST || "localhost", // Load from .env
+    user: process.env.DB_USER || "root", // Load from .env
+    password: process.env.DB_PASS || "Mandir@0711", // Load from .env
+    database: process.env.DB_NAME || "autocure_hub", // Load from .env
     waitForConnections: true,  // Ensures that the pool waits for connections if the limit is reached
     connectionLimit: process.env.DB_CONNECTION_LIMIT || 10, // Load from .env or default to 10
     queueLimit: 0             // No queue limit
 });
 
-// Create a promise-based interface for the connection pool
+// Create a promise-based interface fr the connection pool
 const db = pool.promise();
 
 // Connect and check connection status
